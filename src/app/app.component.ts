f9f74415b7652388db85019f613dee359cd6699f
Muhammad Awais Rafiq <awais.rafiq@keeptruckin.com>
2021-08-03 13:45:36 +0500

    created password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'modern-angular';
+  password:string='';
+  passwordLength:number=0;
+  includeLetters:boolean=false;
+  includeNumbers:boolean=false;
+  includeSymbols:boolean=false;
+  onChangeLengthChange(event:Event){
+    this.passwordLength=parseInt((event.target as HTMLInputElement).value);
+  }
+  onChangeUseLetters(){
+    this.includeLetters=!this.includeLetters;
+  }
+  onChangeUseNumbers(){
+    this.includeNumbers=!this.includeNumbers;
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+  onBtnClick(){
+   const numbers='1234567890';
+   const letters='abcdefghijklmnopqrstuvwxyz';
+   const symbols='~!@#$%^&*()';
+   let validChars='';
+   if(this.includeLetters){
+     validChars+=letters;
+   }
+   if(this.includeNumbers){
+     validChars+=numbers;
+   }
+   if(this.includeSymbols){
+     validChars+=symbols;
+   }
+   let generatedPassword='';
+   for (let i = 0; i < this.passwordLength; i++) {
+     const index = Math.floor(Math.random()* validChars.length);
+     generatedPassword+=validChars[index];
+     
+   }
+   this.password=generatedPassword;
+  }
 }
